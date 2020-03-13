00265c5ab26f76d3deb2c83f8d35fd2bdb8af372
bingbang <44920040@qq.com>
2020-03-14 03:06:03 +0800

    Initial commit

---
+++ src/main/java/com/ipedg/minecraft/clearworld.java
@@ -0,0 +1,17 @@
+package com.ipedg.minecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class clearworld extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+
+    
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
